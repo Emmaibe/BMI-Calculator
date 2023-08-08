06ad8b5974ca24fa8c9956c4a1e5942d6b027bec
lord IBE <122531467+Emmaibe@users.noreply.github.com>
2023-08-08 18:54:55 +0100

    Update Main.java

--- src/main/java/com/lordibe/Main.java
+++ src/main/java/com/lordibe/Main.java
@@ -9,20 +9,22 @@ public class Main {
 
         System.out.println("Check your BMI");
 
-        System.out.println("Your Weight in Pound(lb): ");
-        int weight = scanner.nextInt();
+        System.out.println("Your Weight in Kilogram(kg): ");
+        double weight = scanner.nextDouble();
 
-        System.out.println("Your Height in inches(in): ");
-        int height = scanner.nextInt();
+        System.out.println("Your Height in Meters(m): ");
+        double height = scanner.nextDouble();
 
-        float bmi = (float) weight/(height*height) * 703;
-
-        if (bmi >= 25.0) {
+        double bmi = Math.ceil(weight/Math.pow(height, 2));
+        if (bmi >= 30) {
+            System.out.println("Your BMI is: " + bmi);
+            System.out.println("You are OBESE, Please you need to do something urgently");
+        } else if (bmi >= 25.0 && bmi <= 29.9) {
             System.out.println("Your BMI is: " + bmi);
             System.out.println("You are OVERWEIGHT, Do more work");
         } else if(bmi >= 18.5 && bmi <= 24.9) {
             System.out.println("Your BMI is: " + bmi);
-            System.out.println("You are HEALTHY, Continue the good work");
+            System.out.println("You have a NORMAL WEIGHT, Continue the good work");
         } else {
             System.out.println("Your BMI is: " + bmi);
             System.out.println("You are UNDERWEIGHT, Please eat Healthy");
